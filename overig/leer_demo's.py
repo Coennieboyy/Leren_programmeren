2e3591d7b33138cdb610b0edf4276de3c6678cd9
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-19 11:15:00 +0200

    mapjes

---
+++ overig/leer_demo's.py
@@ -0,0 +1,12 @@
+def optellen(getal1, getal2):
+    totaal = getal1 + getal2
+    return totaal
+
+def aftrekken(getal1, getal2):
+    totaal = getal1 - getal2
+    return totaal
+
+
+antwoord = optellen(5, 7)
+print(antwoord)
+antwoord = aftrekken(antwoord, 4)
