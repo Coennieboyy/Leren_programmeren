c98a70fe7d40fac102bdea7aad0559894ca46090
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-20 11:25:02 +0200

    person opdracht gemaakt

---
+++ module_1/deel_2_userinput/person.py
@@ -0,0 +1,11 @@
+naam = input("hoe heet je? ")
+adres = input("wat is je adres? ")
+postcode = input("wat is je postcode? ")
+woonplaats = input("hoe heet je woonplaats? ")
+
+print("  ----------------------------------------------------")
+print(f"|  naam      : {naam}")
+print(f"|  adres     : {adres}")
+print(f"|  postcode  : {postcode}")
+print(f"|  woonplaats: {woonplaats}")
+print("  ----------------------------------------------------")
