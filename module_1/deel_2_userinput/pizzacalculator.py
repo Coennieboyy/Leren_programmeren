c5dcef8820139790ae94b98e388a52934e1eb018
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-25 10:15:51 +0200

    nieuwe opdracht

--- module_1/deel_2_userinput/pizzacalculator.py
+++ module_1/deel_2_userinput/pizzacalculator.py
@@ -5,6 +5,8 @@ large = 23.80
 inputsmall = int(input("hoeveel small pizza's wil je? "))
 inputmedium = int(input("hoeveel medium pizza's wil je? ")) 
 inputlarge = int(input("hoeveel large pizza's wil je? "))
+antwoord = (inputsmall * small) + (inputmedium * medium) + (inputlarge * large)
+totaal = round(antwoord , 2)
 
 
 print("------------------------------------------")
@@ -12,6 +14,6 @@ print()
 print(f"{inputsmall} small pizza's:  {inputsmall * small}")
 print(f"{inputmedium} medium pizza's: {inputmedium * medium}")
 print(f"{inputlarge} large pizza's:  {inputlarge * large}")
-print(f"totaal:           {(inputsmall * small) + (inputmedium * medium) + (inputlarge * large)}")
+print(f"totaal:           {totaal}")
 print()
 print("------------------------------------------")
