158d48ca2af6cf11adbf4185f76c7fcd5a74dfe6
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-11-15 10:50:43 +0100

    opdracht 2,3,4,5,6

--- module_2/deel_1_een_bereik/forever_young/opdracht_5.py
+++ module_2/deel_1_een_bereik/forever_young/opdracht_5.py
@@ -1,9 +1,18 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
-
+robotArm = RobotArm('exercise 5')
+robotArm.reportFlaws = False
+robotArm.speed = 3
 # Jouw python instructies zet je vanaf hier:
+for i in range(7):
+    robotArm.moveRight()
 
-
+for i in range(8):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    if i < 7:
+        robotArm.moveLeft()
+        robotArm.moveLeft()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
--- module_2/deel_1_een_bereik/forever_young/opdracht_6.py
+++ module_2/deel_1_een_bereik/forever_young/opdracht_6.py
@@ -1,9 +1,19 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
-
+robotArm = RobotArm('exercise 6')
+robotArm.reportFlaws = False
+robotArm.speed = 3
 # Jouw python instructies zet je vanaf hier:
-
+robotArm.moveRight()
+for i in range(3):
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
