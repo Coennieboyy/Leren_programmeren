d8e448bba4c996993f40443bf42e638adb75f925
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-12-11 13:55:04 +0100

    opdrachten robotarm

--- module_2/deel_3_herhaling/forever_young/opdracht_8.py
+++ module_2/deel_3_herhaling/forever_young/opdracht_8.py
@@ -1,20 +1,15 @@
 from RobotArm import RobotArm
-robotArm = RobotArm('exercise 7')
+robotArm = RobotArm('exercise 8')
 robotArm.reportFlaws = False
 robotArm.speed = 3
 # Jouw python instructies zet je vanaf hier:
 robotArm.moveRight()
-for i in range(5):
-    for j in range(6):
-        robotArm.grab()
+for i in range(7):
+    robotArm.grab()
+    for j in range(8):
+        robotArm.moveRight()
+    robotArm.drop()
+    for a in range(8):
         robotArm.moveLeft()
-        robotArm.drop()
-        robotArm.moveRight()
-    if i < 4:
-        robotArm.moveRight()
-        robotArm.moveRight()
-
-    
-
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
---
+++ module_2/deel_3_herhaling/forever_young/opdracht_9.py
@@ -0,0 +1,7 @@
+from RobotArm import RobotArm
+robotArm = RobotArm('exercise 9')
+robotArm.reportFlaws = False
+robotArm.speed = 3
+# Jouw python instructies zet je vanaf hier:
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
